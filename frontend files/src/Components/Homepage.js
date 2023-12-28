011b4760c9cadbcc613e8c79ff724b98c72c5f18
Danni Raphael Morta <drmorta@student.apc.edu.ph>
2023-12-28 16:11:51 +0800

    Frontend Files

---
+++ frontend files/src/Components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+
+const Homepage = () => {
+  return (
+    <div className='home-container'>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ frontend files/src/Data/Constants.js
@@ -0,0 +1,4 @@
+// constants.js
+export const FRAPPE_BASE_URL = "https://acnphilippines.csf.ph";
+export const OAUTH_CLIENT_ID = "edb4f8e388";
+export const OAUTH_CLIENT_SECRET = "4f0ce38022";
